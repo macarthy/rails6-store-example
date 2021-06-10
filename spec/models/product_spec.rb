bac7c3a986f22eaca952d255ae5dc254fe4277ed
Justin MacCarthy <justin@rubystars.biz>
2021-06-10 16:28:00 +0700

    Start of a store project to show various rails + tech skills

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
+describe Product, type: :model do
   context 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:description) }
